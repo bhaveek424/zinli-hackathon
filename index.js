ac4b6935412d2c8eaa53b169bdbe6567e073662e
bhaveek424 <jainbhaveek.v727@gmail.com>
2023-11-19 19:17:05 +0530

    api call

--- index.js
+++ index.js
@@ -9,8 +9,41 @@ const client = new Client({
   ],
 });
 
-client.on('messageCreate', (message) => {
-  message.reply({ content: 'Hello from Bot!' });
+client.on('messageCreate', async (message) => {
+  // Check if the message starts with the verify command
+  if (message.content.startsWith('!verify')) {
+    // Check if a file is attached
+    if (message.attachments.size > 0) {
+      const attachment = message.attachments.first();
+
+      // Download the image
+      const response = await fetch(attachment.url);
+      const imageBuffer = await response.buffer();
+
+      // Send image data to the deepfake detection API
+      const apiKey = process.env.DEEPFAKE_API_KEY;
+      const apiUrl = 'https://api.deepfakedetection.net/v1/verify';
+
+      const apiResponse = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'image/jpeg', // Change content type based on your image type
+          'Api-Key': apiKey,
+        },
+        body: imageBuffer,
+      });
+
+      const result = await apiResponse.json();
+      // Process the result from the API
+      if (result.is_fake) {
+        message.reply('Warning: This image appears to be manipulated.');
+      } else {
+        message.reply('This image seems genuine.');
+      }
+    } else {
+      message.reply('Please attach an image to verify.');
+    }
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
