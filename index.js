ce03fcab1767e33c3b614c2cce3b11b7ae9c3a17
bhaveek424 <jainbhaveek.v727@gmail.com>
2023-11-19 17:07:21 +0530

    nodemon

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from 'express';
 
 const app = express();
 // Get port, or default to 3000
-app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
+// app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
 const PORT = process.env.PORT || 3000;
 
 const client = new Client({
@@ -20,3 +20,11 @@ client.on('messageCreate', (message) => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
